777aaf66658d5335e31c3ef950b7a183e5089306
Shareef shaik <shareefshaik1375@gmail.com>
2020-06-17 16:29:05 +0530

    first_commit

---
+++ preprocessing.py
@@ -0,0 +1,46 @@
+#import all necessary packages
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import nltk
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+stop_words = set(stopwords.words('english'))
+stop_words.remove("not")
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.svm import LinearSVC
+from sklearn.metrics import confusion_matrix,auc,roc_auc_score,f1_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split,RandomizedSearchCV
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scikitplot.metrics import plot_confusion_matrix
+
+from tqdm import tqdm
+import re
+from bs4 import BeautifulSoup
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+def text_Preprocessing(reviews):
+    
+    
+    """ This will clean the text data, remove html tags, remove special characters and then tokenize the reviews to apply Stemmer on each word token."""
+    
+    pre_processed_reviews=[]
+    
+    for review in reviews:
+        review= BeautifulSoup(review,'lxml').getText()    #remove html tags
+        review=re.sub('\S*\d\S*','',review).strip()
+        review=re.sub('[^A-Za-z]+',' ',review)        #remove special chars
+        review=re.sub("n't","not",review)
+        review=word_tokenize(str(review.lower())) #tokenize the reviews into word tokens
+        # now we will split the review into words and then check if these words are in the stop words if so we will remove them, if not we will join
+        review=' '.join(PorterStemmer().stem(word) for word in review if word not in stop_words)
+        pre_processed_reviews.append(review.strip())
+    return pre_processed_reviews
+
+
